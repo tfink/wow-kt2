9725fd75a1f24e1a4f89901a030061141aaab0b1
Torsten Fink <torstenfink@posteo.de>
2025-11-27 16:55:58 +0100

    .

--- src/main/kotlin/de/torstenfink/wow/infrastructure/BNetImportController.kt
+++ src/main/kotlin/de/torstenfink/wow/infrastructure/BNetImportController.kt
@@ -7,21 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping
 class BNetImportController(
     private val bnetClient: BNetClient
 ) {
-    @GetMapping("/bnet/import", produces = ["application/json"])
-    fun bnetImport(): String {
-//        val roster = bnetClient.guildRoster("blackhand", "wild-things")
-//        roster.members
-//            .filter { it.character.level >= 80 }
-//            .forEach { member -> println(member.toString()) }
-        try {
-            val characterProfessions = bnetClient.characterProfessions("blackhand", "hurtspriest")
-            characterProfessions.primaries
-                .forEach { println(it.profession.name) }
-            return "ok"
-        } catch (e: Exception) {
-            println("Error: ${e.message}")
-            return "error"
-        }
+    @GetMapping("/bnet/guild-roster", produces = ["application/json"])
+    fun guildRoster(): String {
+        val roster = bnetClient.guildRoster("blackhand", "wild-things")
+        roster.members
+            .filter { it.character.level >= 80 }
+            .forEach { member -> println(member.toString()) }
 
+        return "ok"
+    }
+
+    @GetMapping("/bnet/character-professions", produces = ["application/json"])
+    fun characterProfessions(): String {
+        val characterProfessions = bnetClient.characterProfessions("blackhand", "hurtspriest")
+        characterProfessions.primaries
+            .forEach { println(it.profession.name) }
+
+        return "ok"
     }
 }
